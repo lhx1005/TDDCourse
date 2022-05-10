288082a6f7297086ae0ec02133e35d23b945d20e
huixinliu <lhx_1005@163.com>
2022-05-10 09:22:56 +0800

    before the refactor

--- lists/tests.py
+++ lists/tests.py
@@ -1,28 +1,36 @@
+from urllib import response
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.views import home_page   #(2)
+from lists.views import home_page  # (2)
 
 # Create your tests here.单元测试
+
+
 class HomePageTest(TestCase):
-    #def test_root_url_resolve_to_home_page_view(self):
-        #found=resolve('/')  #(1)
-        #self.assertEqual(found.func,home_page)  #(1)
-    
-    #def test_home_page_returns_correct_html(self):
-        #response=self.client.get('/')
-        #request=HttpRequest()  #1
-        #response=home_page(request)  #2
-        #html=response.content.decode('utf8')  #3
-        #self.assertTrue(html.startswith('<html>'))  #4
-        #self.assertIn('<title>To-Do lists</title>',html)  #5
-        #self.assertTrue(html.strip().endswith('</html>'))  #4
-        #expected_html=render_to_string('home.html')
-        #self.assertEqual(html,expected_html)
-        #self.assertTemplateUsed(response,'home.html')
+    # def test_root_url_resolve_to_home_page_view(self):
+    # found=resolve('/')  #(1)
+    # self.assertEqual(found.func,home_page)  #(1)
+
+    # def test_home_page_returns_correct_html(self):
+    # response=self.client.get('/')
+    # request=HttpRequest()  #1
+    # response=home_page(request)  #2
+    # html=response.content.decode('utf8')  #3
+    # self.assertTrue(html.startswith('<html>'))  #4
+    # self.assertIn('<title>To-Do lists</title>',html)  #5
+    # self.assertTrue(html.strip().endswith('</html>'))  #4
+    # expected_html=render_to_string('home.html')
+    # self.assertEqual(html,expected_html)
+    # self.assertTemplateUsed(response,'home.html')
 
     def test_uses_home_template(self):
-        response=self.client.get('/')
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response,'home.html')
