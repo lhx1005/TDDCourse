32123cb6d8813d55c116a0de299041a4af2079b6
huixinliu <lhx_1005@163.com>
2022-05-24 08:16:25 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,28 +1,31 @@
 from selenium import webdriver  # (1)
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+from django.test import LiveServerTestCase
+
 import time
 import unittest
 
 # 功能测试
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
+    def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text,[row.text for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.She goes
         # #to check out its homepage
-        self.browser.get("http://localhost:8000")  # (3)
+        self.browser.get(self.live_server_url)  # (3)
 
         # She notices the page title ande header mention to-do list
         self.assertIn(
@@ -56,7 +59,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # The page updates again,and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. The she sees
         # that the site has generated a unique URL for her -- there is some
@@ -68,5 +72,5 @@ class NewVisitorTest(unittest.TestCase):
 
 
 # Satisfied, she goes back to sleep
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+# if __name__ == '__main__':
+#    unittest.main(warnings='ignore')
